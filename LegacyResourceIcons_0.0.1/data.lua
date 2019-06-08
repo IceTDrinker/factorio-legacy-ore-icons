e841265b830f902fe8b4052a294fe188211f7d0b
IceTDrinker <49040125+IceTDrinker@users.noreply.github.com>
2019-06-08 20:14:47 +0200

    Initial mod code

---
+++ LegacyResourceIcons_0.0.1/data.lua
@@ -0,0 +1,37 @@
+--coal
+
+item = data.raw.item["coal"]
+item.icon = "__base__/graphics/icons/coal.png"
+item.dark_background_icon = "__base__/graphics/icons/coal-dark-background.png"
+item.icon_size = 32
+item.icon_mipmaps = 0
+item.pictures = nil
+
+--stone
+item = data.raw.item["stone"]
+item.icon = "__base__/graphics/icons/stone.png"
+item.icon_size = 32
+item.icon_mipmaps = 0
+item.pictures = nil
+
+
+--iron-ore
+item = data.raw.item["iron-ore"]
+item.icon = "__base__/graphics/icons/iron-ore.png"
+item.icon_size = 32
+item.icon_mipmaps = 0
+item.pictures = nil
+
+--copper-ore
+item = data.raw.item["copper-ore"]
+item.icon = "__base__/graphics/icons/copper-ore.png"
+item.icon_size = 32
+item.icon_mipmaps = 0
+item.pictures = nil
+
+--uranium-ore
+item = data.raw.item["uranium-ore"]
+item.icon = "__base__/graphics/icons/uranium-ore.png"
+item.icon_size = 32
+item.icon_mipmaps = 0
+item.pictures = nil
