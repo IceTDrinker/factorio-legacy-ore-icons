ef48fd806fc0cd76d26d61e8e4c3907ef0cd4dce
IceTDrinker <49040125+IceTDrinker@users.noreply.github.com>
2019-06-09 10:11:35 +0200

    Updated to use packaged assets

--- LegacyResourceIcons_0.0.1/data.lua
+++ LegacyResourceIcons_0.0.1/data.lua
@@ -1,15 +1,14 @@
 --coal
-
 item = data.raw.item["coal"]
-item.icon = "__base__/graphics/icons/coal.png"
-item.dark_background_icon = "__base__/graphics/icons/coal-dark-background.png"
+item.icon = "__LegacyResourceIcons__/graphics/icons/coal.png"
+item.dark_background_icon = "__LegacyResourceIcons__/graphics/icons/coal-dark-background.png"
 item.icon_size = 32
 item.icon_mipmaps = 0
 item.pictures = nil
 
 --stone
 item = data.raw.item["stone"]
-item.icon = "__base__/graphics/icons/stone.png"
+item.icon = "__LegacyResourceIcons__/graphics/icons/stone.png"
 item.icon_size = 32
 item.icon_mipmaps = 0
 item.pictures = nil
@@ -17,21 +16,21 @@ item.pictures = nil
 
 --iron-ore
 item = data.raw.item["iron-ore"]
-item.icon = "__base__/graphics/icons/iron-ore.png"
+item.icon = "__LegacyResourceIcons__/graphics/icons/iron-ore.png"
 item.icon_size = 32
 item.icon_mipmaps = 0
 item.pictures = nil
 
 --copper-ore
 item = data.raw.item["copper-ore"]
-item.icon = "__base__/graphics/icons/copper-ore.png"
+item.icon = "__LegacyResourceIcons__/graphics/icons/copper-ore.png"
 item.icon_size = 32
 item.icon_mipmaps = 0
 item.pictures = nil
 
 --uranium-ore
 item = data.raw.item["uranium-ore"]
-item.icon = "__base__/graphics/icons/uranium-ore.png"
+item.icon = "__LegacyResourceIcons__/graphics/icons/uranium-ore.png"
 item.icon_size = 32
 item.icon_mipmaps = 0
 item.pictures = nil
